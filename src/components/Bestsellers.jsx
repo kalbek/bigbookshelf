f12223ce3aeed732aa880205b4a7bcb67b631bcc
KalebBekele <KalebBekele@cbe.com.et>
2023-11-30 14:17:07 +0300

    big bookshelp doers.

--- src/components/Bestsellers.jsx
+++ src/components/Bestsellers.jsx
@@ -6,8 +6,10 @@ export default function Bestsellers() {
     <div className="bg-blackbg w-full my-4 h-auto px-10 pb-20">
       <h1 className="text-white font-medium text-2xl py-10">BESTSELLERS</h1>
       <div className="flex flex-col gap-2 flex-wrap md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {[...Array(10)].map((_, index) => (index !== 6 ? (
-          <HPCards key={0} />
+        {[...Array(10)].map((_, index) => (index !== 4 ? (
+          <div className="lg:flex hidden" key={0}>
+            <HPCards />
+          </div>
         ) : (
           <div key={0}>
             {" "}
