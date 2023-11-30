7fb5239dfe5a77fed5fda1173070673aee8c81ee
KalebBekele <KalebBekele@cbe.com.et>
2023-11-30 12:04:51 +0300

    fix list all books card for popular authors.

--- src/components/PopularAuthors.jsx
+++ src/components/PopularAuthors.jsx
@@ -7,7 +7,7 @@ const PopularAuthors = () => (
     <div className="bg-blackbg w-full my-4 h-auto px-10 pb-20">
       <h1 className="text-3xl text-white mb-6">POPULAR AUTHORS</h1>
       <div className="flex flex-col gap-2 flex-wrap md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {[...Array(10)].map((_, index) => (index !== 6 ? (
+        {[...Array(10)].map((_, index) => (index !== 9 ? (
           <PopularAuthorCards key={0} />
         ) : (
           <div key={0}>
