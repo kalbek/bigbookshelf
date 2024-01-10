85495083fbfd9f1e92dfd161bba5cc47ed0c118b
KalebBekele <Nonst00pCbe0912>
2024-01-10 10:41:26 +0300

    Fix margins for catagory dropdown.

--- src/components/productcomponents/Top.jsx
+++ src/components/productcomponents/Top.jsx
@@ -1,7 +1,7 @@
 const Top = () => (
   <div className="w-full flex flex-col md:flex-row justify-between px-4 md:px-8 py-10 bg-blackbg items-start">
     {/* top filter results and tags */}
-    <div className="flex justify-between  w-full pl-4">
+    <div className="flex justify-between  w-full px-4">
       <div className="flex gap-10 justify-center items-center">
         <div className="flex flex-col md:flex-row gap-2">
           <p className="text-white text-xl md:text-3xl">FILTER</p>
@@ -34,7 +34,7 @@ const Top = () => (
     <select
       name=""
       id=""
-      className="bg-inherit text-white flex p-2 md:mr-10 mt-4 md:mt-0 w-full justify-between outline outline-white outline-2 rounded-[15px] md:w-[30%] md:h-[70px] cursor-pointer text-lg  "
+      className="bg-inherit text-white flex p-2 md:mr-20 mt-4 md:mt-0 w-full justify-between outline outline-white outline-2 rounded-[15px] md:w-[30%] md:h-[70px] cursor-pointer text-lg  "
     >
       <option
         className="bg-zinc-700 text-gray-200 font-semibold py-4 text-lg"
