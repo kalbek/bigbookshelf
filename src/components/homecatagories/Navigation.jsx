5d67770d125b396d64d2986fd18f6b8d8bfb2299
KalebBekele <Nonst00pCbe0912>
2024-01-10 09:32:01 +0300

    Add close icon to mobile menu.

--- src/components/homecatagories/Navigation.jsx
+++ src/components/homecatagories/Navigation.jsx
@@ -1,6 +1,23 @@
 const Navigation = () => (
   <>
     <header className="w-[97vw] mx-auto bg-blackbg pt-6 pb-4 items-center w-full rounded-2xl mt-8 px-6 overflow-hidden">
+      <div className="absolute w-full h-[100vh] left-0 top-0 z-50 bg-[#F4F4F4] p-6">
+        <div className="relative rounded w-full h-full bg-blackbg outline-black outline-2">
+          <div className="absolute top-1 right-1 scale-[0.3] ">
+            <img src="close-icon.svg" alt="close icon" />
+          </div>
+          <div className="absolute bottom-6 left-5">
+            <img src="logo.svg" alt="logo" className="scale-[100%]" />
+          </div>
+          <ul>
+            <li>Books</li>
+            <li>Authors</li>
+            <li>What&nbsp;to&nbsp;Read?</li>
+            <li>Gift&nbsp;Ideas</li>
+            <li>About&nbsp;Us</li>
+          </ul>
+        </div>
+      </div>
       <div className="container mx-auto gap-4 flex-spaced-start">
         <img
           src="logo.svg"
@@ -11,7 +28,6 @@ const Navigation = () => (
           <ul className="hidden lg:mt-2 lg:flex md:grid md:grid-cols-3 lg:gap-[1.5vw] md:gap-1 text-white   text-[1.4rem] font-light">
             <li className="">
               <a href="./products">Books</a>
-              {" "}
             </li>
             <li className="">Authors</li>
             <li className="">What&nbsp;to&nbsp;Read?</li>
@@ -20,7 +36,7 @@ const Navigation = () => (
           </ul>
         </nav>
         {/* icons and login */}
-        <div className="hidden md:flex flex-col lg:flex-row md:flex-col gap-2  lg:gap-12  z-50">
+        <div className="hidden md:flex flex-col lg:flex-row md:flex-col gap-2  lg:gap-12  z-40">
           {/* icon sets */}
           <ul className="flex items-start flex-centered gap-8 ">
             <li>
