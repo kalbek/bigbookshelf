4a55f98f3238c076d38ac02463a8d43e8a37f5d9
KalebBekele <Nonst00pCbe0912>
2024-01-11 10:31:10 +0300

    Add you may also like.

--- src/components/pages/show/AlsoLike.jsx
+++ src/components/pages/show/AlsoLike.jsx
@@ -4,16 +4,19 @@ import BookCard from "../utilities/BookCard";
 export default function AlsoLike() {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-        {BookList.slice(0, 5).map((book, index) => (
-          <div key={0}>
-            <BookCard
-              image={book.image}
-              title={book.title}
-              author={book.author}
-            />
-          </div>
-        ))}
+      <div className="flex flex-col justify start">
+        <p className="text-3xl font-semibold text-verydarkgray p-4 ml-4"> YOU MAY ALSO LIKE</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+          {BookList.slice(0, 5).map((book, index) => (
+            <div key={0}>
+              <BookCard
+                image={book.image}
+                title={book.title}
+                author={book.author}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
