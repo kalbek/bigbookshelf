b78f916f0f57378413ee034aa9d063de7dc592b9
KalebBekele <Nonst00pCbe0912>
2024-01-12 16:46:31 +0300

    Initialized readers review section.

---
+++ src/components/pages/show/ReadersReviews.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ReadersReviews = () => {
+  return (
+    <div>
+      <div className="something">hellow ...</div>
+      <div className="something">hellow ...</div>
+    </div>
+  );
+};
+
+export default ReadersReviews;
---
+++ src/components/pages/show/Reviews.js
@@ -0,0 +1,24 @@
+export const Reviews = [
+    {
+        img: "",
+        name: "Geoffrey Mott",
+        starCount: 4,
+        description: "",
+        date: "",
+    },
+    {
+        img: "",
+        name: "Lisa Anderson",
+        starCount: 1,
+        description: "",
+        date: "",
+    },
+    {
+        img: "",
+        name: "Lisa Anderson",
+        starCount: 5,
+        description: "",
+        date: "",
+    },
+   
+]
