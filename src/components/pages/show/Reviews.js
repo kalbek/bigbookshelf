7c5fdec6a3798ee365593c731ccce7e5f7224690
kalbek <access2kaleb@gmail.com>
2024-01-30 16:50:53 +0300

    Populate reviews data.

--- src/components/pages/show/Reviews.js
+++ src/components/pages/show/Reviews.js
@@ -3,21 +3,34 @@ const Reviews = [
     img: "",
     name: "Geoffrey Mott",
     starCount: 4,
-    description: "",
-    date: "",
+    description:
+      "Muscle by Alan Trotter is a fresh piece of noir fiction. Our main characters are the two crooks Box and _____. They are on the hunt for odd jobs that most of us, hopefully, would not be willing to fulfill. It felt reminiscent of both A Clockwork Orange and The Time Machine. I guess that can seem like a strange combination, but Alan Trotter made it work.When Box and _____. encounter a private detective and this strange writer, events quickly start to unravel. Oh, and there is a girl of course!",
+    date: "10 October 2022",
   },
   {
     img: "",
     name: "Lisa Anderson ",
     starCount: 1,
-    description: "",
-    date: "",
+    description:
+      "Oh I really disliked this in the end. What middling interest I had was completely lost by the fifth and last section.This is the story of two tough guys, goons for hire, as they pick up odd jobs roughing up guys, collecting debts and breaking hands. All written as a kind of Chandler pastiche, but possibly more of a parody.One of the goons, Box, passes the time reading sci-fi stories from pulp magazines an acquaintance writes for. Then, as the story progresses and the pair's jobs take a dark turn",
+    date: "10 October 2022",
   },
   {
     img: "",
-    name: "Lisa Anderson",
+    name: "Florencio Dorrance",
     starCount: 5,
-    description: "",
-    date: "",
+    description:
+      "This is fantastic, and could have been written just for me. Grimly funny absurdist-existentialist noir that incorporates pulp sci-fi as well as hardboiled crime fiction elements to become a surprisingly powerful examination of the lasting impact of male violence in its many forms.It also does one of my favourite things, which is to recount other fictional stories within its own pages, a Vonnegut-style technique I never seem to tire of. The ending, too, which some have complained about, I think works perfectly.Phenomenally entertaining and sneakily profound.",
+    date: "10 October 2022",
+  },
+  {
+    img: "",
+    name: "Anne Powel",
+    starCount: 5,
+    description:
+      "Read a great review in the Guardian and took a chance. I should have paid more attention to the comparison with The New York Trilogy, my least favourite Auster experience.This does noir tropes really well with women always called “twists” or “frails”, but I wasn’t really on board for the final section where it all gets a bit meta. I’m down for literary noir, but this was no Motherless Brooklyn.",
+    date: "10 October 2022",
   },
 ];
+
+export default Reviews;
--- src/components/pages/utilities/ReveiwsCard.jsx
+++ src/components/pages/utilities/ReveiwsCard.jsx
@@ -18,15 +18,14 @@ export default function ReviewsCard({
           <p className="text-xl">{date}</p>
         </div>
         <div className="flex justify-between">
-          <div className="flex gap-1"></div>
+          <div className="flex gap-1">
+            <img src="staradust.svg" alt="" />
+            <p className="text-xl">{reveiwRating}</p>
+          </div>
           &nbsp;
         </div>
-        
-        <div className="text-philippinegray text-xl">
-          <p>{author}</p>
-        </div>
-        <div className="text-xl text-verydarkgray">
-          <p>{title}</p>
+        <div className="w-full outline ouline-2">
+          <p className="text-lg">{review}</p>
         </div>
       </div>
     </>
