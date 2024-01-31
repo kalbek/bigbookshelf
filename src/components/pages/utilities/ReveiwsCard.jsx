200c9cc529cd004687ebb9b52f0d2d0046967680
kalbek <access2kaleb@gmail.com>
2024-01-31 16:27:40 +0300

    Just a commit.

--- src/components/pages/utilities/ReveiwsCard.jsx
+++ src/components/pages/utilities/ReveiwsCard.jsx
@@ -12,7 +12,7 @@ export default function ReviewsCard({
       <div className="relative bg-white padding-4 flex flex-col items-start gap-2 p-6 rounded-2xl w-auto">
         <div className="flex justify-between">
           <div className="flex gap-1">
-            <img src={blob} alt="book" className="w-[100%] " />
+            <img src={blob} alt="book" className="w-[100%]" />
             <p className="text-xl">{reviewerName}</p>
           </div>
           <p className="text-xl">{date}</p>
